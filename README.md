de7dc7b8993af2645c8f52ac2d6aff8a8a3da6b2
Jose38782 <A01666969@tec.mx>
2025-03-21 12:33:13 -0600

    Memory.py ya cuenta con un contador de taps y lo muestra ademas de centrar los textos que estan dentro de los cuadrados del tablero

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Juego de Memorama Modificado
+# Codigo de memory.py para mostrar taps y centrar las letras en el tablero
--- memory.py
+++ memory.py
@@ -1,13 +1,26 @@
+"""Memory, puzzle game of number pairs.
+
+Exercises:
+
+1. Count and print how many taps occur.
+2. Decrease the number of tiles to a 4x4 grid.
+3. Detect when all tiles are revealed.
+4. Center single-digit tile.
+5. Use letters instead of tiles.
+"""
+
 from random import *
 from turtle import *
-
 from freegames import path
+import string
 
 car = path('car.gif')
-tiles = list(range(32)) * 2
+alphabet = list(string.ascii_uppercase)
+lower_letters = ['a','b','c','d','e','f']
+tiles = alphabet + lower_letters + alphabet + lower_letters
 state = {'mark': None}
 hide = [True] * 64
-
+taps = 0 # contador
 
 def square(x, y):
     """Draw white square with black outline at (x, y)."""
@@ -34,9 +47,13 @@ def xy(count):
 
 def tap(x, y):
     """Update mark and hidden tiles based on tap."""
+    
+    global taps
     spot = index(x, y)
     mark = state['mark']
 
+    if hide[spot]:
+        taps += 1
     if mark is None or mark == spot or tiles[mark] != tiles[spot]:
         state['mark'] = spot
     else:
@@ -51,20 +68,28 @@ def draw():
     goto(0, 0)
     shape(car)
     stamp()
+    #mostrar contador en el juego
+    up()
+    goto(0, 220)
+    color('black')
+    write(f'TAPS: {taps}', align='center', font=('Comic Sans', 24, 'bold'))
+
 
     for count in range(64):
         if hide[count]:
             x, y = xy(count)
             square(x, y)
 
+
     mark = state['mark']
 
     if mark is not None and hide[mark]:
         x, y = xy(mark)
         up()
-        goto(x + 2, y)
+        goto(x + 25, y )
         color('black')
-        write(tiles[mark], font=('Arial', 30, 'normal'))
+        # para centrar los numeros en el memorama
+        write(tiles[mark], align ="center", font=('Arial', 30, 'normal'))
 
     update()
     ontimer(draw, 100)
